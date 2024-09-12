ef7da9d28044ab263c564020d7adfbeacfa3e329
João Lucas <introducaoteste@gmail.com>
2024-09-12 08:18:45 -0300

    Chamando o componente no app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,18 @@
-import { Component } from '@angular/core'; 
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+
+import { Message } from './messages/message.model'; 
+import { MessageComponent } from './messages/message.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MessageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
-  title = 'frontend';
+  title = 'frontend'
+  messageBinding: Message = new Message("Texto da Messagem", "Lucas");
 }
