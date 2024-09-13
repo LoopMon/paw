099b599c4cd600261073f906052a309d4d84faba
João Lucas <introducaoteste@gmail.com>
2024-09-13 15:03:07 -0300

    Utilização do ngFor, ngIf, ngSwitch e @for

--- src/app/messages/message.component.ts
+++ src/app/messages/message.component.ts
@@ -1,16 +1,22 @@
 import { FormsModule } from "@angular/forms";
 import { Component, EventEmitter, Input, input, Output } from "@angular/core";
+import { CommonModule } from "@angular/common";
 import { Message } from "./message.model";
 
 @Component({
     selector: 'app-message',
     standalone: true,
-    imports: [FormsModule],
+    imports: [FormsModule, CommonModule],
     templateUrl: './message.component.html',
     styleUrl: './message.component.css'
 })
 
+// ngStyle -> CommonModule
+// ngModel -> FomrsModule
+
 export class MessageComponent {
+
+    color = 'yellow'
 
     // @Input() messageVarClasse: Message = new Message("", "");
     
