d477ed2a33dd01fcdc48422e59767e0fa76536e9
João Lucas <introducaoteste@gmail.com>
2024-09-13 15:04:08 -0300

    Adicição de novos elementos na página

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 import { Message } from './messages/message.model'; 
 import { MessageComponent } from './messages/message.component';
@@ -7,12 +8,29 @@ import { MessageComponent } from './messages/message.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MessageComponent],
+  imports: [RouterOutlet, MessageComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 
+// ngFor -> CommonModule
+
 export class AppComponent {
-  title = 'frontend'
+  title = 'frontend';
   messageBinding: Message = new Message("Texto da Messagem", "Lucas");
+
+  messageS: Message[] = [
+    new Message("Bom dia", "Carlos"),
+    new Message("Boa tarde", "Luiz"),
+    new Message("Boa noite", "Julio")
+  ]
+
+  mostrarElemento: boolean = true;
+  onMudaMostrarElemento() {
+    this.mostrarElemento = !this.mostrarElemento;
+  }
+
+  valorNgSwitch: number = 0;
 }
+
+// slide 18 do b2
