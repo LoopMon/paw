bec4c33cabeff2f8cc2bf48da713a4d4e1e386a8
João Lucas <introducaoteste@gmail.com>
2024-10-16 15:49:22 -0300

    Criação do componente de autenticação

---
+++ src/app/auth/authentication.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: "app-authentication",
+    standalone: true,
+    template: `
+        <h1> Componente de Autenticação </h1>
+    `
+})
+
+export class AuthenticationComponent {}
