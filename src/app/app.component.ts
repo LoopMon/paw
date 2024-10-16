5db80f85ab67468954ec923c026c68ce73090dbd
João Lucas <introducaoteste@gmail.com>
2024-10-16 15:51:36 -0300

    Reestruturando componente e chamada do header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,35 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
-import { Message } from './messages/message.model'; 
-import { MessageListComponent } from './messages/message-list.component';
-import { MessageInputComponent } from './messages/message-input.component';
+import { HeaderComponent } from './header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MessageListComponent, MessageInputComponent],
+  imports: [RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 
-// ngFor -> CommonModule
-
 export class AppComponent {
-  title = 'frontend';
-  messageBinding: Message = new Message("Texto da Messagem", "Lucas");
-
-  messageS: Message[] = [
-    new Message("Bom dia", "Carlos"),
-    new Message("Boa tarde", "Luiz"),
-    new Message("Boa noite", "Julio")
-  ]
-
-  mostrarElemento: boolean = true;
-  onMudaMostrarElemento() {
-    this.mostrarElemento = !this.mostrarElemento;
-  }
-
-  valorNgSwitch: number = 0;
+  title="Programação Avançada para Web"
 }
-// slide 29 do b2
+// slide 4c1
