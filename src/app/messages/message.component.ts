b5c4510fc22a515a2426e8fce16e6002c85ec5d5
João Lucas <introducaoteste@gmail.com>
2024-10-23 11:22:41 -0300

    Chamando as funcionalidades do service para deletar e atualizar

--- src/app/messages/message.component.ts
+++ src/app/messages/message.component.ts
@@ -14,17 +14,41 @@ import { MessageService } from "./message.services";
 
 export class MessageComponent {   
     messageVarClasse = input<Message>(new Message("", ""));
-       
+    possoEditar = false
+    messageEdit = ""
     @Output() outputMessage = new EventEmitter<string>();
 
     constructor(private messageServiceObj: MessageService) {}
     
     onEdit() {
-        this.outputMessage.emit("Retorno: msg filha p/ o app pai (signal)");
+        this.possoEditar = !this.possoEditar;
+        this.messageEdit = this.messageVarClasse().content;
+    }
+
+    onConfirmEdit() {
+        this.messageVarClasse().content = this.messageEdit;
+        this.possoEditar = !this.possoEditar;
+
+        this.messageServiceObj.updateMessage(this.messageVarClasse(), { content:this.messageVarClasse().content })
+            .subscribe({
+                next: (response) => {
+                    console.log("Mensagem atualizada com sucesso:", response);
+                },
+                error: (error) => {
+                    console.error("Erro ao atualizar a mensagem:", error);
+                }
+            })
     }
 
     onDelete() {
-        this.messageServiceObj.deleteMessage(this.messageVarClasse())
+        this.messageServiceObj.deleteMessage(this.messageVarClasse()).subscribe({
+            next: (response) => {
+                console.log("Mensagem deletada com sucesso:", response);
+            },
+            error: (error) => {
+                console.error("Erro ao deletar a mensagem:", error);
+            }
+        });
     }
 }
 
