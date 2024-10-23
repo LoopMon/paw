38ba3cf6f77bc6ed1cd4e54b88eb88330852612b
João Lucas <introducaoteste@gmail.com>
2024-10-23 11:17:57 -0300

    Chamando o UserService para o provider

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,11 +5,13 @@ import { provideHttpClient } from '@angular/common/http';
 import { routes } from './app.routes';
 
 import { MessageService } from './messages/message.services';
+import { UserService } from './auth/user.services';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideHttpClient(),
-    { provide: MessageService }
+    { provide: MessageService },
+    { provide: UserService }
   ]
 };
